e107c7515bba6a253a84cc01f878c9b824ba2481
XUranus <2257238649wdx@gmail.com>
2023-01-18 08:45:13 +0800

    first commit

--- FileIOUtil.cpp
+++ FileIOUtil.cpp
@@ -2,16 +2,162 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+namespace {
+constexpr auto WIN32_UID = 0;
+constexpr auto WIN32_GID = 0;
 }
-
 
 namespace FileIO {
 
-uint64_t OpenDirResult::Size() const
+#ifdef LINUX
+StatResult::StatResult(const struct stat& statbuff)
+{
+	memcpy_s(&m_stat, sizeof(struct stat), &statbuff, sizeof(struct stat));
+}
+#endif
+
+#ifdef WIN32
+StatResult::StatResult(const BY_HANDLE_FILE_INFORMATION& handleFileInformation)
+{
+	memcpy_s(&m_handleFileInformation, sizeof(BY_HANDLE_FILE_INFORMATION),
+		&handleFileInformation, sizeof(BY_HANDLE_FILE_INFORMATION));
+}
+#endif
+
+uint64_t StatResult::UserID() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_gid);
+#endif
+#ifdef WIN32
+	return WIN32_UID;
+#endif
+}
+
+uint64_t StatResult::GroupID() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_uid);
+#endif
+#ifdef WIN32
+	return WIN32_GID;
+#endif
+}
+
+uint64_t StatResult::AccessTime() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_atime);
+#endif
+#ifdef WIN32
+	DWORD low = m_handleFileInformation.ftLastAccessTime.dwLowDateTime;
+	DWORD high = m_handleFileInformation.ftLastAccessTime.dwHighDateTime;
+	return low + (MAXDWORD + 1) * high;
+#endif
+}
+
+uint64_t StatResult::CreationTime() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_ctime);
+#endif
+#ifdef WIN32
+	DWORD low = m_handleFileInformation.ftCreationTime.dwLowDateTime;
+	DWORD high = m_handleFileInformation.ftCreationTime.dwHighDateTime;
+	return low + (MAXDWORD + 1) * high;
+#endif
+}
+
+uint64_t StatResult::ModifyTime() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_mtime);
+#endif
+#ifdef WIN32
+	DWORD low = m_handleFileInformation.ftLastWriteTime.dwLowDateTime;
+	DWORD high = m_handleFileInformation.ftLastWriteTime.dwHighDateTime;
+	return low + (MAXDWORD + 1) * high;
+#endif
+}
+
+uint64_t StatResult::UniqueID() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_ino);
+#endif
+#ifdef WIN32
+	DWORD low = m_handleFileInformation.nFileIndexLow;
+	DWORD high = m_handleFileInformation.nFileIndexHigh;
+	return low + (MAXDWORD + 1) * high;
+#endif
+}
+
+uint64_t StatResult::Size() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_size);
+#endif
+#ifdef WIN32
+	DWORD low = m_handleFileInformation.nFileSizeLow;
+	DWORD high = m_handleFileInformation.nFileSizeHigh;
+	return low + (MAXDWORD + 1) * high;
+#endif
+}
+
+uint64_t StatResult::DeviceNmber() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_rdev);
+#endif
+#ifdef WIN32
+	return static_cast<uint64_t>(m_handleFileInformation.dwVolumeSerialNumber);
+#endif
+}
+
+uint64_t StatResult::LinksNumber() const
+{
+#ifdef LINUX
+	return static_cast<uint64_t>(m_stat.st_rdev);
+#endif
+#ifdef WIN32
+	return static_cast<uint64_t>(m_handleFileInformation.nNumberOfLinks);
+#endif
+}
+
+bool StatResult::IsDirectory() const
+{
+#ifdef WIN32
+	return (m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
+#endif
+#ifdef LINUX
+	return (m_stat.st_mode & S_IFDIR) != 0;
+#endif
+}
+
+#ifdef WIN32
+bool StatResult::IsArchive() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE; }
+bool StatResult::IsCompressed() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_COMPRESSED; }
+bool StatResult::IsEncrypted() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_ENCRYPTED; }
+bool StatResult::IsSparseFile() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_SPARSE_FILE; }
+bool StatResult::IsHidden() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN; }
+bool StatResult::IsOffline() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_OFFLINE; }
+bool StatResult::IsReadOnly() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_READONLY; }
+bool StatResult::IsSystem() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM; }
+bool StatResult::IsTemporary() const { return m_handleFileInformation.dwFileAttributes & FILE_ATTRIBUTE_TEMPORARY; }
+#endif
+
+#ifdef LINUX
+bool StatResult::IsRegular() const { return (m_stat.m_mode & S_IFREG) != 0; }
+bool StatResult::IsPipe() const { return (m_stat.m_mode & S_IFFIFO) != 0; }
+bool StatResult::IsCharDevice() const { return (m_stat.m_mode & S_IFCHR) != 0; }
+bool StatResult::IsBlockDevice() const { return (m_stat.m_mode & S_IFBLK) != 0; }
+bool StatResult::IsSymLink() const { return (m_stat.m_mode & S_IFLNK) != 0; }
+bool StatResult::IsSocket() const { return (m_stat.m_mode & S_IFSOCK) != 0; }
+#endif
+
+
+
+uint64_t OpenDirEntry::Size() const
 {
 #ifdef WIN32
 	DWORD low = m_findFileData.nFileSizeLow;
@@ -31,50 +177,15 @@ uint64_t OpenDirResult::Size() const
 
 
 #ifdef WIN32
-bool OpenDirResult::IsArchive() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE;
-}
-
-bool OpenDirResult::IsCompressed() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_COMPRESSED;
-}
-
-bool OpenDirResult::IsEncrypted() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_ENCRYPTED;
-}
-
-bool OpenDirResult::IsSparseFile() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_ENCRYPTED;
-}
-
-bool OpenDirResult::IsHidden() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN;
-}
-
-bool OpenDirResult::IsOffline() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_OFFLINE;
-}
-
-bool OpenDirResult::IsReadOnly() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_READONLY;
-}
-
-bool OpenDirResult::IsSystem() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM;
-}
-
-bool OpenDirResult::IsTemporary() const
-{
-	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_TEMPORARY;
-}
+bool OpenDirEntry::IsArchive() const {	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE; }
+bool OpenDirEntry::IsCompressed() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_COMPRESSED; }
+bool OpenDirEntry::IsEncrypted() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_ENCRYPTED; }
+bool OpenDirEntry::IsSparseFile() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_SPARSE_FILE; }
+bool OpenDirEntry::IsHidden() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN; }
+bool OpenDirEntry::IsOffline() const {	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_OFFLINE; }
+bool OpenDirEntry::IsReadOnly() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_READONLY; }
+bool OpenDirEntry::IsSystem() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM; }
+bool OpenDirEntry::IsTemporary() const { return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_TEMPORARY; }
 #endif
 
 
@@ -82,32 +193,32 @@ bool OpenDirResult::IsTemporary() const
 
 
 #ifdef LINUX
-bool OpenDirResult::IsUnknown() const
+bool OpenDirEntry::IsUnknown() const
 {
 	return (m_dirent->d_type & DT_KNOWN != 0);
 }
 
-bool OpenDirResult::IsPipe() const
+bool OpenDirEntry::IsPipe() const
 {
 	return (m_dirent->d_type & DT_FIFO != 0);
 }
 
-bool OpenDirResult::IsCharDevice() const
+bool OpenDirEntry::IsCharDevice() const
 {
 	return (m_dirent->d_type & DT_CHR != 0);
 }
 
-bool OpenDirResult::IsBlockDevice() const
+bool OpenDirEntry::IsBlockDevice() const
 {
 	return (m_dirent->d_type & DT_BLK != 0);
 }
 
-bool OpenDirResult::IsSymLink() const
+bool OpenDirEntry::IsSymLink() const
 {
 	return (m_dirent->d_type & DT_LNK != 0);
 }
 
-bool OpenDirResult::IsSocket() const
+bool OpenDirEntry::IsSocket() const
 {
 	return (m_dirent->d_type & DT_SOCK != 0);
 }
@@ -118,11 +229,11 @@ bool OpenDirResult::IsSocket() const
 
 
 
-uint64_t OpenDirResult::INode()
+uint64_t OpenDirEntry::INode()
 {
 #ifdef WIN32
 	if (DoStat()) {
-		return std::static_cast<uint64_t>(m_stat.st_ino);
+		return static_cast<uint64_t>(m_stat.st_ino);
 	} else {
 		return 0;
 	}
@@ -132,7 +243,7 @@ uint64_t OpenDirResult::INode()
 #endif
 }
 
-bool OpenDirResult::IsNormal() const
+bool OpenDirEntry::IsNormal() const
 {
 #ifdef WIN32
 	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_NORMAL;
@@ -142,7 +253,7 @@ bool OpenDirResult::IsNormal() const
 #endif
 }
 
-bool OpenDirResult::IsDirectory() const
+bool OpenDirEntry::IsDirectory() const
 {
 #ifdef WIN32
 	return m_findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
@@ -152,7 +263,7 @@ bool OpenDirResult::IsDirectory() const
 #endif
 }
 
-std::string OpenDirResult::Name() const
+std::string OpenDirEntry::Name() const
 {
 #ifdef WIN32
 	return std::string(m_findFileData.cFileName);
@@ -162,7 +273,7 @@ std::string OpenDirResult::Name() const
 #endif
 }
 
-uint64_t OpenDirResult::AccessTime() const
+uint64_t OpenDirEntry::AccessTime() const
 {
 #ifdef WIN32
 	DWORD low = m_findFileData.ftLastAccessTime.dwLowDateTime;
@@ -171,14 +282,14 @@ uint64_t OpenDirResult::AccessTime() const
 #endif
 #ifdef LINUX
 	if (DoStat()) {
-		return std::static_cast<uint64_t>(m_stat.st_atime);
+		return static_cast<uint64_t>(m_stat.st_atime);
 	else {
 		return 0;
 	}
 #endif
 }
 
-uint64_t OpenDirResult::CreationTime() const
+uint64_t OpenDirEntry::CreationTime() const
 {
 #ifdef WIN32
 	DWORD low = m_findFileData.ftCreationTime.dwLowDateTime;
@@ -187,14 +298,14 @@ uint64_t OpenDirResult::CreationTime() const
 #endif
 #ifdef LINUX
 	if (DoStat()) {
-		return std::static_cast<uint64_t>(m_stat.st_ctime);
+		return static_cast<uint64_t>(m_stat.st_ctime);
 	else {
 		return 0;
 	}
 #endif
 }
 
-uint64_t OpenDirResult::ModifyTime() const
+uint64_t OpenDirEntry::ModifyTime() const
 {
 #ifdef WIN32
 	DWORD low = m_findFileData.ftLastWriteTime.dwLowDateTime;
@@ -203,66 +314,40 @@ uint64_t OpenDirResult::ModifyTime() const
 #endif
 #ifdef LINUX
 	if (DoStat()) {
-		return std::static_cast<uint64_t>(m_stat.st_mtime);
+		return static_cast<uint64_t>(m_stat.st_mtime);
 	else {
 		return 0;
 	}
 #endif
 }
 
-bool OpenDirResult::DoStat()
+bool OpenDirEntry::DoStat()
 {
 	if (m_stated) {
 		return true;
 	}
-#ifdef WIN32
-	const std::string separator = "\\";
-#else
+#ifdef LINUX
 	const std::string separator = "/";
-#endif
 	std::string fullpath = m_dirPath + separator + Name();
 	if (stat(fullpath.c_str(), &m_stat) < 0) {
 		m_statFailed = true;
 		return false;
 	}
 	m_stated = true;
-}
-
-
-
-
-
-
-std::optional<OpenDirResult> OpenDir(const std::string& path)
-{
+#endif
 #ifdef WIN32
-	OpenDirResult iterator;
-	iterator.m_dirPath = path;
-	iterator.m_fileHandle = ::FindFirstFile(path.c_str(), &iterator.m_findFileData);
-	if (iterator.m_fileHandle == INVALID_HANDLE_VALUE) {
-		return std::nullopt;
-	}
-	return std::optional(iterator);
-#endif
 
-#ifdef LINUX
-	OpenDirResult iterator;
-	iterator.m_dirPath = path;
-	DIR* dir = ::opendir(path.c_str());
-	if (dir == nullptr) {
-		return std::nullopt;
-	}
-	iterator.m_dir = dir;
-	iterator.m_dirent = readdir(dir);
-	if (iterator.m_dirent == nullptr) {
-		return std::nullopt;
-	}
-	return std::optional(iterator);
 #endif
-	return std::nullopt;
 }
 
-bool OpenDirResult::Next()
+
+
+
+
+
+
+
+bool OpenDirEntry::Next()
 {
 	memset(&m_stat, 0, sizeof(struct stat));
 	m_stated = false;
@@ -287,7 +372,7 @@ bool OpenDirResult::Next()
 }
 
 
-void OpenDirResult::Close()
+void OpenDirEntry::Close()
 {
 #ifdef WIN32
 	if (m_fileHandle != nullptr && m_fileHandle != INVALID_HANDLE_VALUE) {
@@ -295,6 +380,68 @@ void OpenDirResult::Close()
 		m_fileHandle = nullptr;
 	}
 #endif
+}
+
+
+
+
+
+
+
+
+std::optional<StatResult> Stat(const std::string& path)
+{
+#ifdef LINUX
+	struct stat statbuff {};
+	if (::stat(path.c_str(), &statbuff) < 0) {
+		return std::nullopt;
+	}
+	return std::make_optional<StatResult>(statbuff);
+#endif
+#ifdef WIN32
+	BY_HANDLE_FILE_INFORMATION handleFileInformation{};
+	OFSTRUCT ofStruct;
+	HANDLE hFile = ::CreateFile(path.c_str(), GENERIC_READ,
+		FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, 0);
+	if (hFile == nullptr || hFile == INVALID_HANDLE_VALUE) {
+		return std::nullopt;
+	}
+	if (::GetFileInformationByHandle(hFile, &handleFileInformation) == 0) {
+		::CloseHandle(hFile);
+		return std::nullopt;
+	}
+	::CloseHandle(hFile);
+	return std::make_optional<StatResult>(handleFileInformation);
+#endif
+}
+
+std::optional<OpenDirEntry> OpenDir(const std::string& path)
+{
+#ifdef WIN32
+	OpenDirEntry iterator;
+	iterator.m_dirPath = path;
+	iterator.m_fileHandle = ::FindFirstFile(path.c_str(), &iterator.m_findFileData);
+	if (iterator.m_fileHandle == INVALID_HANDLE_VALUE) {
+		return std::nullopt;
+	}
+	return std::make_optional(iterator);
+#endif
+
+#ifdef LINUX
+	OpenDirEntry iterator;
+	iterator.m_dirPath = path;
+	DIR* dir = ::opendir(path.c_str());
+	if (dir == nullptr) {
+		return std::nullopt;
+	}
+	iterator.m_dir = dir;
+	iterator.m_dirent = readdir(dir);
+	if (iterator.m_dirent == nullptr) {
+		return std::nullopt;
+	}
+	return std::optional(iterator);
+#endif
+	return std::nullopt;
 }
 
 }
--- FileIOUtil.h
+++ FileIOUtil.h
@@ -17,8 +17,71 @@
 
 namespace FileIO {
 
+class StatResult {
+public:
+#ifdef LINUX
+	StatResult(const struct stat& statbuff);
+#endif
 
-class OpenDirIterator
+#ifdef WIN32
+	StatResult(const BY_HANDLE_FILE_INFORMATION& handleFileInformation);
+#endif
+	/**
+	* user id and group id of the owner, always set to zero on windows
+	*/
+	uint64_t UserID() const;
+	uint64_t GroupID() const;
+
+	/**
+	* AccessTime and CreationTime are invalid on FAT32 drives
+	*/
+	uint64_t AccessTime() const;
+	uint64_t CreationTime() const;
+	uint64_t ModifyTime() const;
+
+	/**
+	* UNIX fs hardlink file share the same inode, but has no meaning on FAT32/HPFS/NTFS.. fs
+	*/
+	uint64_t UniqueID() const;
+
+	uint64_t Size() const; // size in bytes
+	uint64_t DeviceNmber() const; // no of the disk containning the file
+	uint64_t LinksNumber() const; // no of hard links to the file, on WIN32 always set to 1 on non-NTFS fs
+
+	bool IsDirectory() const;
+
+#ifdef WIN32
+	bool IsArchive() const;
+	bool IsCompressed() const;
+	bool IsEncrypted() const;
+	bool IsSparseFile() const;
+	bool IsHidden() const;
+	bool IsOffline() const;
+	bool IsReadOnly() const;
+	bool IsSystem() const;
+	bool IsTemporary() const;
+#endif
+
+#ifdef LINUX
+	bool IsRegular() const;
+	bool IsPipe() const;
+	bool IsCharDevice() const;
+	bool IsBlockDevice() const;
+	bool IsSymLink() const;
+	bool IsSocket() const;
+#endif
+
+private:
+#ifdef LINUX
+	struct stat m_stat {};
+#endif
+#ifdef WIN32
+	BY_HANDLE_FILE_INFORMATION m_handleFileInformation{};
+#endif
+};
+
+
+class OpenDirEntry
 {
 public:
 	bool Next();
@@ -55,12 +118,15 @@ public:
 	bool IsSocket() const;
 #endif
 
+	// forbid copy
+	OpenDirEntry(const OpenDirEntry&) = delete;
+	OpenDirEntry operator = (const OpenDirEntry&) = delete;
+
 private:
 	std::string m_dirPath;
 	bool m_stated = false;
 	bool m_statFailed = false;
 #ifdef WIN32
-	struct stat m_stat {};
 	HANDLE m_fileHandle = nullptr;
 	WIN32_FIND_DATA m_findFileData;
 #endif
@@ -70,11 +136,12 @@ private:
 	DIR* m_dir;
 	struct dirent* m_dirent;
 #endif
-
-	// forbid copy
-	OpenDirIterator(const OpenDirIterator&) = delete;
-	OpenDirIterator operator = (const OpenDirIterator&) = delete;
 };
+
+
+
+std::optional<StatResult> Stat(const std::string& path);
+std::optional<OpenDirEntry> OpenDir(const std::string& path);
 }
 
 #endif
@@ -88,4 +155,11 @@ private:
  https://blog.csdn.net/lj19990824/article/details/120046241
  https://www.bbsmax.com/A/E35poVvgJv/
  https://www.writebug.com/git/Carryme/FileSearch
-*/
+ https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/stat-functions?view=msvc-170
+ https://learn.microsoft.com/en-us/cpp/c-runtime-library/stat-structure-st-mode-field-constants?view=msvc-170
+ https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants
+ https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfileinformationbyhandle
+ https://learn.microsoft.com/en-us/windows/win32/api/fileapi/ns-fileapi-by_handle_file_information
+ https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants
+ https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfileinformationbyhandle
+ */
